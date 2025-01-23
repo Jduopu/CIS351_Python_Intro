521deabf307ae505947a99e1fec58fb2070a138a
Jduopu <Jduopu3112@gmail.com>
2025-01-23 08:54:45 -0500

    First upload for CIS 351

---
+++ concat.py
@@ -0,0 +1,12 @@
+name = input("What is your name? ")
+print("Hi there, " + name)
+print(name + ", Welcome to our website")
+print("Hi, " + name + ", let me make sure your name is " + name + "?")
+
+email = input("What is your email? ")
+nickname = input("What is your nickname? ")
+address = input("What is your address? ")
+print("Is this correct, " + email + " " + nickname + " " + address)
+
+age = int(input("Please enter your age: "))
+print("You are", age ,"years old")
